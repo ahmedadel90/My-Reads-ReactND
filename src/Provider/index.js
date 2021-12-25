d0a382ffedc26a68c0d6eb26c01c051b7d4dadea
Ahmed Adel <ahmed.adel.ezzat.90@gmail.com>
2021-12-25 04:32:16 +0200

    Update index.js

--- src/Provider/index.js
+++ src/Provider/index.js
@@ -15,16 +15,32 @@ export default class index extends Component {
                 const wantToRead = books.filter(bookZ => bookZ.shelf === "wantToRead");
                 this.setState({ books, currentlyReading, read, wantToRead });
             },
-            updateBook: (book, newShelf, allShelfs) => {
+    //        updateBook: (book, newShelf, allShelfs) => {
+    //            const updatedBooks = this.state.books.map(BOOKZ => {
+    //                const findID = allShelfs[newShelf].find(
+    //                    bookID => bookID === BOOKZ.id);
+    //                if (findID) {
+    //                    BOOKZ.shelf = newShelf;
+    //               } return BOOKZ;
+    //           });
+    //            this.state.addBooks(updatedBooks);
+    //        }
+
+
+            updateBook: (book, newShelf, allShelfs, books) => {
                 const updatedBooks = this.state.books.map(BOOKZ => {
-                    const findID = allShelfs[newShelf].find(
-                        bookID => bookID === BOOKZ.id);
-                    if (findID) {
-                        BOOKZ.shelf = newShelf;
-                    } return BOOKZ;
+                        for (var i =0;i < 100 ; i ++)
+                        {
+                            if(book.id===BOOKZ.id)
+                            {
+                                BOOKZ.shelf = newShelf; 
+                            }return BOOKZ;
+                        };
                 });
                 this.state.addBooks(updatedBooks);
             }
+
+
         };
     }
 
